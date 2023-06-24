e76d59ac9d569d4d4230e7fd2f184379b51de5fb
fahrur-rizal <62198976+fahrur-rizal@users.noreply.github.com>
2023-06-25 06:32:07 +0700

    congratulation_ayang

---
+++ login.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+st.set_page_config(
+   page_title="Anikmatillah Trisnayanti, S.EI.",
+   page_icon="㊗️",
+   layout="centered",
+   initial_sidebar_state="auto",
+)
+
+
+def authent(username, password):
+    import pandas as pd
+    df_user = pd.read_csv('user.csv')
+    if username == df_user['user'][0] and password ==str(df_user['password'][0]):
+        return True
+    return False
+
+def login():
+    st.title("Login Page")
+    st.write("Silakan masuk menggunakan username dan password Anda.")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if authent(username, password):
+            st.session_state.login_status = True
+            st.success("Login berhasil! dan klik tombol login sekali lagi untuk masuk utama")
+            # Tandai status login sebagai berhasil
+        else:
+            st.error("Username atau password salah.")
+
+
+if 'login_status' not in st.session_state:
+    st.session_state.login_status = False
+
+if  not st.session_state.login_status:
+    login()
+else:
+    import video
+    video.app
+            
+
---
+++ video.py
@@ -0,0 +1,23 @@
+import streamlit as st
+def add_custom_css():
+    st.markdown('<link rel="stylesheet" href="assets/styles.css">', unsafe_allow_html=True)
+
+# Memanggil fungsi untuk menambahkan CSS kustom
+add_custom_css()
+def app():
+    st.title('Congratulation Anikmatillah Trisnayanti, S.EI. 💖')
+    st.write('Selamat atas kelulusan sidangnya ayang, maaf aku gak bisa kasih apa-apa ke ayang, cuman bisa buat kayak gini. Aku harap ayang happy setelah lihat halaman ini, halaman website ini khusus aku buat untuk ayang yang memuat video ucapan selamat atas kelulusan sidangnya ayang. Untuk melihat videonya klik tombol lhat video!')
+    if st.button('lihat video'):
+        st.subheader('Video ini aku persembahkan buat ayang')
+        video_file = open('sayang.mp4', 'rb')
+        video_bytes = video_file.read()
+        st.video(video_bytes, format="video/mp4")
+        from streamlit_extras.let_it_rain import rain
+
+        rain(
+            emoji="💝",
+            font_size=54,
+            falling_speed=5,
+            animation_length="infinite",
+        )
+app()
