58c18f95e69464be55db6fa44fa539acf23a9091
fahrur-rizal <62198976+fahrur-rizal@users.noreply.github.com>
2023-06-25 07:26:30 +0700

    Update video.py

--- video.py
+++ video.py
@@ -6,7 +6,7 @@ def add_custom_css():
 add_custom_css()
 def app():
     st.title('Congratulation Anikmatillah Trisnayanti, S.EI. 💖')
-    st.write('Selamat atas kelulusan sidangnya ayang, maaf aku gak bisa kasih apa-apa ke ayang, cuman bisa buat kayak gini. Aku harap ayang happy setelah lihat halaman ini, halaman website ini khusus aku buat untuk ayang yang memuat video ucapan selamat atas kelulusan sidangnya ayang. Untuk melihat videonya klik tombol lhat video!')
+    st.write('Selamat atas kelulusan sidangnya ayang, maaf aku gak bisa kasih apa-apa ke ayang, cuman bisa buat kayak gini. Aku harap ayang happy setelah lihat halaman ini, halaman website ini khusus aku buat untuk ayang yang memuat video ucapan selamat atas kelulusan sidangnya ayang. Untuk melihat videonya klik tombol lihat video!')
     if st.button('lihat video'):
         st.subheader('Video ini aku persembahkan buat ayang')
         video_file = open('sayang.mp4', 'rb')
